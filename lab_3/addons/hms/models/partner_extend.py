3e5747255ed2b0ece537ffd8216af81a4c243406
Yousef Elsayed <yousef4511@outlook.com>
2024-06-11 18:46:30 +0300

    Lab3: Add website link field to tree view for customers

--- lab_3/addons/hms/models/partner_extend.py
+++ lab_3/addons/hms/models/partner_extend.py
@@ -1,7 +1,24 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     related_patient_id = fields.Many2one("hms.patient", string="Related Patient")
+
+    @api.constrains("related_patient_id")
+    def _check_related_patient_email(self):
+        for record in self:
+            if record.related_patient_id:
+                existing_partner = self.env["res.partner"].search(
+                    [
+                        ("related_patient_id", "=", record.related_patient_id.id),
+                        ("id", "!=", record.id),
+                    ]
+                )
+                if existing_partner:
+                    raise ValidationError(
+                        "The related patient is already linked to another customer with email: %s"
+                        % record.related_patient_id.email
+                    )
